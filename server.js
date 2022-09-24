2c1daaa4fc4fa93659dc1fac1192837052a79ca8
HAMMBE <benjamin.hamm7@gmail.com>
2022-09-24 13:31:59 +0200

    fix: Deploy to heroku

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 
-//Install express server
 const express = require('express');
-const path = require('path');
-
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/tp02_HAMM_Benjamin'));
+app.use(express.static('./dist/'));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/tp02_HAMM_Benjamin/index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile('index.html', { root: 'dist/' });
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
