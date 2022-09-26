c007d3f1faabd68368d4906de209f304974eab2e
HAMMBE <benjamin.hamm7@gmail.com>
2022-09-26 21:17:05 +0200

    fix: deploy to heroku

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(express.static('./dist/'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/pi-project'));
 
-app.get('/*', (req, res) => {
-  res.sendFile('index.html', { root: 'dist/' });
+app.get('/*', function(req,res) {
+
+  if(req.url === '/') {
+    res.sendFile(path.join(__dirname,'/dist/tp02-hamm-benjamin/index.html'));
+  }
+  else {
+    res.sendFile(path.join(__dirname, '/dist/tp02-hamm-benjamin' + req.url))
+  }
+
 });
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
