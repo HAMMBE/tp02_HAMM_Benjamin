a2c51c8c1201705d93093f42649f3eb4792488bb
HAMMBE <benjamin.hamm7@gmail.com>
2022-11-11 14:41:30 +0100

    feat: added button to product detail

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -4,6 +4,8 @@ import {ProductService} from "../service/product.service";
 import {Observable} from "rxjs";
 import {Product} from "../../Types/product";
 import {map} from "rxjs/internal/operators/map";
+import {AddProduct} from "../../shared/actions/cart-action";
+import {Store} from "@ngxs/store";
 
 @Component({
   selector: 'app-product-detail',
@@ -13,12 +15,15 @@ import {map} from "rxjs/internal/operators/map";
 export class ProductDetailComponent implements OnInit {
   id : number = 0;
   product: Product | undefined;
-  constructor(private route : ActivatedRoute, private productService : ProductService ){ }
+  constructor(private route : ActivatedRoute, private productService : ProductService, private store : Store ){ }
 
   ngOnInit(): void {
     this.id = Number(this.route.snapshot.paramMap.get('id'));
     this.productService.getProducts().subscribe((data) => {
       this.product = data.find(p => p.id == this.route.snapshot.params['id']);
     });
+  }
+  addToCart(product: Product) : void {
+    this.store.dispatch(new AddProduct(product));
   }
 }
